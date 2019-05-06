37300eebb2f4ffeb223e68bed53375f3f6b6e626
NosaPrecious <Okundaye.preciousnosa@gmail.com>
2019-05-06 15:51:57 -0400

    new commit

--- mallmart/app/controllers/customers_controller.rb
+++ mallmart/app/controllers/customers_controller.rb
@@ -1,10 +1,42 @@
 class CustomersController < ApplicationController
+  before_action :find_id, only: [:show, :edit, :update]
   def index
 
   end
 
   def show
     @customer = Customer.find(params[:id])
+
   end
+
+def new
+  @customer= Customer.new
+end
+
+def create
+@customer = Customer.create(strong_params)
+end
+
+def edit
+end
+
+def update
+  @customer.update(strong_params)
+  redirect_to customer_path(@customer)
+end
+
+def destroy
+  Customer.destroy(params[:id])
+end
+
+private
+
+def find_id
+  @customer = Customer.find(params[:id])
+end
+
+def strong_params
+  params.require(:customer).permit!
+end
 
 end
--- mallmart/app/controllers/inventories_controller.rb
+++ mallmart/app/controllers/inventories_controller.rb
@@ -1,2 +1,11 @@
 class InventoriesController < ApplicationController
+
+def index
+@inventories= Inventory.all
+end
+
+def show
+@inventory= Inventories.find(params[:id])
+end
+
 end
