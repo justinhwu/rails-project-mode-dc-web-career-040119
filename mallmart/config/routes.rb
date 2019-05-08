b2d63f951baa0a35bc608f01db210129f9fdc1e6
Justin <justinhwu95@gmail.com>
2019-05-07 21:22:29 -0400

    Need to fix creating a new account

--- mallmart/config/routes.rb
+++ mallmart/config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
-  delete "/logout", to: "sessions#destroy", as: "logout"
+
   resources :suppliers
   resources :inventories
   resources :purchases
   resources :customers
   get '/customers/new', to: 'customers#show'
+
+  delete "/logout", to: "sessions#destroy", as: "logout"
   post '/inventories/:id', to: 'purchases#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
